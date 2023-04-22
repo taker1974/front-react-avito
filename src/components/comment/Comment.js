b331103c10d000230ae54d6c835e10051fee1964
Dmitry Bizin <bizin.mitya@mail.ru>
2023-04-22 21:09:27 +0400

    Revert "Trying to fix"
    This reverts commit 812e785196390d90dc946d44bdfa752cf35016cc.

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -3,7 +3,6 @@ import api from "../../utils/api";
 
 import Buttons from "../buttons/Buttons";
 import EditCommentPopup from "../editCommentPopup/EditCommentPopup";
-import useImgFromSecureArea from "../../utils/hooks/useSecureData";
 
 function Comment({
     text,
@@ -37,15 +36,14 @@ function Comment({
         deleteComment1(adId, commentId);
     };
 
-    let userImage = useImgFromSecureArea(img, username, password);
     return (
         <li className="comment" key={commentId}>
             <div className="comment-box">
-                style={{
-                  userImage: `url(${
-                      userImage ? userImage : 'src/images/greg-rakozy-oMpAz-DN-9I-unsplash.jpg'
-                  })`,
-                }}
+                {img ? (
+                    <img src={`http://localhost:8080` + img} alt="user-img" className="comment-img"/>
+                ) : (
+                    <img src={'/src/images/greg-rakozy-oMpAz-DN-9I-unsplash.jpg'} alt="user-img" className="comment-img"/>
+                )}
                 <p className="comment-text comment__author-text">
                     {authorName || 'Комментатор'}
                     <span>{commentDate.toLocaleString()}</span>
